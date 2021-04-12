5dd86be7448f849afb9b5bfbd6ce8703914f694e
hmuronaka <capriccio27@gmail.com>
2021-04-12 23:16:10 +0900

    popupjs message通信箇所にtry-catchを追加(ChromeExtensionのエラーログに記録されなくなるので良いのか判断つきかねる)

--- share_url/popup/popup.js
+++ share_url/popup/popup.js
@@ -46,21 +46,29 @@ class Popup {
 
   /** テキストを選択されているRoomに投稿する */
   async sendTextToRoom() {
-    if( !this.#selectedRoom ) {
-      return;
+    try {
+      if( !this.#selectedRoom ) {
+         return;
+      }
+      let message = {
+        type: 'send-text-to-room',
+        roomUrl: this.#selectedRoom.value,
+        text: this.#textarea.value.trim()
+      };
+      await this.#sendMessageToChatwork(message);
+    } catch(error) {
+      console.log(error);
     }
-    let message = {
-      type: 'send-text-to-room',
-      roomUrl: this.#selectedRoom.value,
-      text: this.#textarea.value.trim()
-    };
-    this.#sendMessageToChatwork(message);
   }
 
   /** ルーム一覧を取得してselectを更新する */
   async getRoomNames() {
-    let res = await this.#sendMessageToChatwork({ type: 'get-room-names' });
-    this.#refreshRoomSelect( res.rooms );
+    try {
+      let res = await this.#sendMessageToChatwork({ type: 'get-room-names' });
+      this.#refreshRoomSelect( res.rooms );
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
